2231619a212283ff97f070704aeb7711c6e86a70
Aleksandr Borysenko <borysenko343@gmail.com>
2023-10-09 08:34:50 +0300

    fix
    fix

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -9,7 +9,7 @@ class Component<T extends ComponentProps> {
   constructor (public props:T) {}
 }
 
-class Page extends Component<{ title: string }> {
+class Page extends Component<ComponentProps> {
   pageInfo() {
     console.log(this.props.title);
   }
